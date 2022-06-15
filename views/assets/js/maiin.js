704f80f6b6afbac3f0c695bf007fc269c6358fee
hanamanse <hana9589@gmail.com>
2022-06-16 06:39:09 +0900

    메인페이지 - 새 글 쓰기 메뉴 추가. 새 글 쓰기 html/css 초안

--- views/assets/js/maiin.js
+++ views/assets/js/maiin.js
@@ -1,36 +1,3 @@
-
-/* 알림 보기 */
-let $noti = $(".headerNoti");
-let $notiBox = $(".notiHidden");
-let $notiClose = $(".notiXbtn");
-
-$noti.click(function(){
-    $notiBox.toggle();
-});
-
-
-
-/* 아이콘 클릭시 */
-let $headerIcon = $(".headerIcon");
-let $iconBox = $(".iconHidden");
-
-$headerIcon.click(function(){
-    $iconBox.toggle();
-
-    if(this.focusout){
-        if($iconBox.css('display','block')){
-            console.log("포커스 아웃");
-            $iconBox.css('display','none');
-            $iconBox.addClass('hidden');
-        }
-    }
-});
-
-/* $headerIcon.focusout(function(){
-    console.log("포커스 아웃");
-    $iconBox.css('display','none');
-}); */
-
 /* 카테고리 필터 선택 */
 let $cateListArr = $(".cateLi");
 
