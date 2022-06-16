3f385f0bb4003c33defdbdb80dc016f2ae1dd111
hanamanse <hana9589@gmail.com>
2022-06-17 07:04:21 +0900

    어드민 페이지 - 메인 (초안) / 스터디 목록,상세보기

--- views/assets/js/header.js
+++ views/assets/js/header.js
@@ -2,32 +2,22 @@
 /* 새 글 쓰기 */
 const $writeBtn = $(".writeBtn");
 const $writeBox = $(".writeHidden");
-let writeCheck = false;
-
 
 $writeBtn.click(function(){
-    $writeBox.toggle();
-});
-
-/* function focusOut(){
-    if($writeBox.css("display") == "block"){
-        console.log("writeBox열림");
-        
-        $('html').click(function(e){
-            console.log(e);
-            if(!$writeBox.has(e.target).length){
-                $writeBox.css("display","none");
-            }
-        });
-    };
-} */
-
-/* $("html").click(function(e){
-    console.log(e);
-    if(!$(e.target).hasClass('writeHidden')){
-        console.log("포커스 아웃");
+    if(!$writeBox.hasClass("open")){
+        $writeBox.addClass("open");
+    } else{
+        $writeBox.removeClass("open");
     }
-}); */
+
+    $('html').mouseup(function(e){
+        if($writeBox.hasClass("open")){
+            if($writeBox.has($(e.target)).length == 0){
+                $writeBox.removeClass("open");
+            }
+        }
+    })
+});
 
 /* 알림 보기 */
 let $noti = $(".headerNoti");
@@ -35,7 +25,19 @@ let $notiBox = $(".notiHidden");
 let $notiClose = $(".notiXbtn");
 
 $noti.click(function(){
-    $notiBox.toggle();
+    if(!$notiBox.hasClass("open")){
+        $notiBox.addClass("open");
+    } else{
+        $notiBox.removeClass("open");
+    }
+
+    $('html').mouseup(function(e){
+        if($notiBox.hasClass("open")){
+            if($notiBox.has($(e.target)).length == 0){
+                $notiBox.removeClass("open");
+            }
+        }
+    })
 });
 
 /* 아이콘 클릭시 */
@@ -43,18 +45,17 @@ let $headerIcon = $(".headerIcon");
 let $iconBox = $(".iconHidden");
 
 $headerIcon.click(function(){
-    $iconBox.toggle();
-
-    if(this.focusout){
-        if($iconBox.css('display','block')){
-            console.log("포커스 아웃");
-            $iconBox.css('display','none');
-            $iconBox.addClass('hidden');
-        }
+    if(!$iconBox.hasClass("open")){
+        $iconBox.addClass("open");
+    } else{
+        $iconBox.removeClass("open");
     }
-});
 
-/* $headerIcon.focusout(function(){
-    console.log("포커스 아웃");
-    $iconBox.css('display','none');
-}); */
+    $('html').mouseup(function(e){
+        if($iconBox.hasClass("open")){
+            if($iconBox.has($(e.target)).length == 0){
+                $iconBox.removeClass("open");
+            }
+        }
+    })
+});
