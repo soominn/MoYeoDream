f1bb2310abf3502790015bae372444ab3380c28f
soominn <chosm0129@naver.com>
2022-06-17 21:24:44 +0900

    인텔리제이 개발환경 업로드

---
+++ spring/moyeodream/src/main/java/com/project/moyeodream/MoyeodreamApplication.java
@@ -0,0 +1,13 @@
+package com.project.moyeodream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeodreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoyeodreamApplication.class, args);
+    }
+
+}
---
+++ spring/moyeodream/src/test/java/com/project/moyeodream/MoyeodreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moyeodream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeodreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
