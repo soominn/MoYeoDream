ac28308329284b52b91a2ec1baf368d6aed471f3
haeyeon <yeon99016@gmail.com>
2022-06-18 20:16:31 +0900

    게시글 / 댓글 1차

---
+++ spring/moyeodream/src/main/java/com/project/moyeodream/controller/CommentController.java
@@ -0,0 +1,25 @@
+package com.project.moyeodream.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/comment/*")
+public class CommentController {
+    // 댓글 작성
+
+    // 댓글 수정
+
+    // 댓글 삭제
+
+    // 내 댓글 목록
+    @GetMapping("list")
+    public String myComment(){
+        return "myPage/myComment";
+    }
+}
