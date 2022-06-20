eacd3dcd8fab63dc5daea10ad88faa13fec8e6e7
csw <csw980928@gmail.com>
2022-06-20 21:06:50 +0900

    vo항목 추가

--- spring/moyeodream/src/main/java/com/project/moyeodream/domain/vo/CommentVO.java
+++ spring/moyeodream/src/main/java/com/project/moyeodream/domain/vo/CommentVO.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Component;
 @Data
 public class CommentVO {
     private Integer commentNumber;                   // 댓글 고유번호(DB SEQUENCE 사용)
+    private String commentContent;                   // 댓글 내용
     private String commentRegisterDate;              // 댓글 작성날짜(DB SYSDATE 사용)
     private String commentUpdateDate;                // 댓글 수정날짜(DB SYSDATE 사용)
-    private String commentContent;                   // 댓글 내용
     private Integer commentMemberNumber;             // 댓글 멤버 넘버(FK)    -   댓글 작성자 정보 가져오기 위해
+    private Integer commentPostNumber;               // 댓글 멤버 넘버(FK)    -   댓글의 게시글 정보 가져오기 위해
+
 }
