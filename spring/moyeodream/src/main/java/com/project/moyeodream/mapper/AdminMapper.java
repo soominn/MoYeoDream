3ac2f34393c68981b24cad6d2456e4ae842dded8
csw <csw980928@gmail.com>
2022-06-25 16:25:47 +0900

    admin login - 추가

--- spring/moyeodream/src/main/java/com/project/moyeodream/mapper/AdminMapper.java
+++ spring/moyeodream/src/main/java/com/project/moyeodream/mapper/AdminMapper.java
@@ -6,5 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper //스프링 마이바티스에서 xml파일과 연결될 Mapper 인터페이스임을 알려준다.
 public interface AdminMapper {
     // 관리자 로그인
-    public int login(AdminVO adminVO);
+    public Integer login(AdminVO adminVO);
+    // 관리자 로그인
+    public String id(String id);
 }
--- spring/moyeodream/src/main/resources/static/js/adminLogin.js
+++ spring/moyeodream/src/main/resources/static/js/adminLogin.js
@@ -21,17 +21,60 @@ $loginBtn.click(function(){
         if(!$inputPw.val()){ // 비밀번호 입력 안했을때
             $error.text("비밀번호를 입력해주세요.");
 
-        }else if($inputId.val() != "admin"){ // 아이디 불일치
-            $error.text("아이디를 확인해주세요.");
-
-        }else if ($inputId.val() == "admin"){
-            if($inputPw.val() != "pw1234"){ // 비밀번호 불일치
-                $error.text("비밀번호를 확인해주세요.");
-
-            }else if($inputPw.val() == "pw1234"){ // 로그인 성공
-                $loginForm.submit();
-                console.log("로그인 성공");
-            }
         }
+        // else if(/*$inputId.val() != "admin"*/id()){ // 아이디 불일치
+            // $error.text("아이디를 확인해주세요.");
+
+        // }else if ($inputId.val() == "admin"){
+        //     if($inputPw.val() != "pw1234"){ // 비밀번호 불일치
+        //         $error.text("비밀번호를 확인해주세요.");
+        //
+        //     }else if($inputPw.val() == "pw1234"){ // 로그인 성공
+        //         $loginForm.submit();
+        //         console.log("로그인 성공");
+        //     }
+        // }
     }
 });
+
+// function id() {
+//     $.ajax({
+//         url:"admin/id",
+//         type: "post",
+//         data: {id: "$inputId.val()"},
+//         success: function(result){
+//             if(result == null){
+//                 $error.text("아이디를 확인해주세요.");
+//             }else {
+//                 $error.text("");
+//             }
+//         },
+//         error: function(request, status, error){
+//             console.log("실패..");
+//             console.log(request);
+//             console.log(status);
+//             console.log(error);
+//         }
+//     });
+// }
+//
+// function login() {
+//     $.ajax({
+//         url:"admin/login",
+//         type: "post",
+//         data: {adminId: "$inputId.val()", adminPw : "$inputId.val()"},
+//         success: function(result){
+//             if(result == 0){
+//                 $error.text("비밀번호를 확인해주세요.");
+//             }else {
+//                 $loginForm.submit();
+//             }
+//         },
+//         error: function(request, status, error){
+//             console.log("실패..");
+//             console.log(request);
+//             console.log(status);
+//             console.log(error);
+//         }
+//     });
+// }
