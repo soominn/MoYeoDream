38473847336a6f78fb03645f423b4ac3a697e9bc
inseoksong <inseok04162@naver.com>
2022-06-25 16:52:39 +0900

    채용공고, 문의하기 - 1차

--- spring/moyeodream/src/main/java/com/project/moyeodream/controller/JobPostingController.java
+++ spring/moyeodream/src/main/java/com/project/moyeodream/controller/JobPostingController.java
@@ -1,18 +1,25 @@
 package com.project.moyeodream.controller;
 
 import com.project.moyeodream.domain.vo.JobpostingVO;
+import com.project.moyeodream.service.JobpostingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/jobPosting/*")
 public class JobPostingController {
+    private final JobpostingService jobpostingService;
 
     //    모든 채용 공고 목록
     @GetMapping("list")
@@ -22,21 +29,87 @@ public class JobPostingController {
     @GetMapping("approveList")
     public void jobPostApprove(){}
 
+
     //    채용 공고 조회
     @GetMapping("read")
-    public void jobPostRead(Integer jobPostingNumber){}
+    public String jobPostRead(Integer jobpostingNumber, Integer jobpostingMemberNumber, Model model){
+        log.info("----------------------------");
+        log.info("jobpostingRead............. : " + jobpostingNumber);
+        log.info("jobpostingMemberRead............. : " + jobpostingMemberNumber);
+        log.info("----------------------------");
+        jobpostingService.jobpostVisit(jobpostingNumber);
+        model.addAttribute("jobPosting", jobpostingService.jobpostRead(jobpostingNumber, jobpostingMemberNumber));
+        return "/jobPosting/jobPostingView";
+    }
+
+
+    //    수정 페이지에서 공고 내용 가져오기
+    @GetMapping("modifyRead")
+    public String inquiryModifyRead(Integer jobpostingNumber, Integer jobpostingMemberNumber, Model model){
+        log.info("----------------------------");
+        log.info("jobpostingRead............. : " + jobpostingNumber);
+        log.info("jobpostingMemberRead............. : " + jobpostingMemberNumber);
+        log.info("----------------------------");
+        model.addAttribute("jobPosting", jobpostingService.jobpostRead(jobpostingNumber, jobpostingMemberNumber));
+        return "/jobPosting/jobPostingModify";
+    }
 
     //    채용 공고 작성
     @PostMapping("register")
-    public void jobPostRegister(JobpostingVO jobpostingVO){}
+    public RedirectView jobPostRegister(JobpostingVO jobpostingVO, RedirectAttributes rttr){
+        log.info("----------------------------");
+        log.info("register............. : " + jobpostingVO);
+        log.info("----------------------------");
+
+        // 세션에서 넘어온 유저 정보가 있어야 사용할 수 있음
+        // HttpSession session = req.getSession();
+        // Integer memberNumber = (Integer)session.getAttribute("memberNumber");
+        // jobpostingVO.setJobpostingMemberNumber(memberNumber);
+
+        // 유저 정보 받아오는테스트
+        jobpostingVO.setJobpostingMemberNumber(1);
+        //-----------------------------------
+
+        jobpostingService.jobpostRegister(jobpostingVO);
+        rttr.addFlashAttribute("jobpostingNumber", jobpostingVO.getJobpostingNumber());
+
+        return new RedirectView("/main");
+    }
 
     //    채용 공고 수정
     @PostMapping("modify")
-    public void jobPostModify(JobpostingVO jobpostingVO){}
+    public RedirectView jobPostModify(JobpostingVO jobpostingVO, RedirectAttributes rttr){
+        log.info("----------------------------");
+        log.info("modify............. : " + jobpostingVO);
+        log.info("----------------------------");
+
+        // 세션에서 넘어온 유저 정보가 있어야 사용할 수 있음
+        // HttpSession session = req.getSession();
+        // Integer memberNumber = (Integer)session.getAttribute("memberNumber");
+        // jobpostingVO.setJobpostingMemberNumber(memberNumber);
+
+        // 유저 정보 받아오는테스트
+        jobpostingVO.setJobpostingMemberNumber(1);
+        //-----------------------------------
+
+        jobpostingService.jobpostUpdate(jobpostingVO);
+
+        rttr.addAttribute("jobpostingNumber", jobpostingVO.getJobpostingNumber());
+        rttr.addAttribute("jobpostingMemberNumber", jobpostingVO.getJobpostingMemberNumber());
+
+        return new RedirectView("/jobPosting/read");
+    }
 
     //    채용 공고 삭제
     @GetMapping("remove")
-    public void jobPostRemove(Integer jobPostingNumber){}
+    public RedirectView jobPostRemove(int jobpostingNumber, RedirectAttributes rttr){
+        log.info("----------------------------");
+        log.info("remove............. : " + jobpostingNumber);
+        log.info("----------------------------");
+
+        jobpostingService.jobpostRemove(jobpostingNumber);
+        return new RedirectView("/main");
+    }
 
     //    채용 공고 승인
     @GetMapping("approve")
--- spring/moyeodream/src/main/java/com/project/moyeodream/domain/dao/InquiryDAO.java
+++ spring/moyeodream/src/main/java/com/project/moyeodream/domain/dao/InquiryDAO.java
@@ -1,5 +1,7 @@
 package com.project.moyeodream.domain.dao;
 
+import com.project.moyeodream.domain.vo.Criteria;
+import com.project.moyeodream.domain.vo.InquiryDTO;
 import com.project.moyeodream.domain.vo.InquiryVO;
 import com.project.moyeodream.mapper.InquiryMapper;
 import lombok.RequiredArgsConstructor;
@@ -10,16 +12,26 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class InquiryDAO {
-    private InquiryMapper inquiryMapper;
+    private final InquiryMapper inquiryMapper;
 
     // 전체 문의보기
-    public List<InquiryVO> getTotalList(){
-        return inquiryMapper.getTotalList();
+    public List<InquiryDTO> getTotalList(Criteria criteria){
+        return inquiryMapper.getTotalList(criteria);
+    }
+
+    // 게시판 글 개수
+    public int getTotal(){
+        return inquiryMapper.getTotal();
     }
 
     // 문의 상세 조회
-    public InquiryVO getList(int inquiryNumber){
+    public InquiryDTO getList(int inquiryNumber){
         return inquiryMapper.getList(inquiryNumber);
+    }
+
+    // 스터디 만든 회원의 닉네임 찾기
+    public String findNickName(int inquiryMemberNumber) {
+        return inquiryMapper.selectNickName(inquiryMemberNumber);
     }
 
     // 문의하기 작성
@@ -35,6 +47,11 @@ public class InquiryDAO {
     // 문의하기 삭제
     public boolean inquiryDelete(int inquiryNumber){
         return inquiryMapper.delete(inquiryNumber) == 1;
+    }
+
+    // 조회수 증가
+    public boolean inquiryVisit(int inquiryNumber){
+        return inquiryMapper.visit(inquiryNumber) == 1;
     }
 
     // 문의하기 답변
