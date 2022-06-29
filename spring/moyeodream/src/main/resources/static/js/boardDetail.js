642bc96a0234b9afd8629bc5f1910c008854311b
hanamanse <hana9589@gmail.com>
2022-06-30 05:29:58 +0900

    게시판 - 댓글 등록 js / html 반영

--- spring/moyeodream/src/main/resources/static/js/boardDetail.js
+++ spring/moyeodream/src/main/resources/static/js/boardDetail.js
@@ -1,16 +1,31 @@
 
 let postCommentService = (function(){
 
-    // 댓글 불러오기
+    // 댓글 목록 불러오기
     function getList(postNumber, callback){
-        console.log("getList.....................");
         $.getJSON("/postReply/list/"+ postNumber, function(list){
-            console.log("js : "+ list);
             if(callback){
                 callback(list);
             }
         });
     }
 
-return {getList:getList};
+    // 댓글 등록
+    function register(param, callback){
+        console.log("js : register.....");
+        $.ajax({
+            url : "/postReply/register",
+            type : "post",
+            data : JSON.stringify(param),
+            contentType: "application/json",
+            success : function(){
+                if(callback){
+                    console.log("댓글 등록 성공");
+                    callback();
+                }
+            }
+        });
+    }
+
+return {getList: getList, register: register};
 })();
