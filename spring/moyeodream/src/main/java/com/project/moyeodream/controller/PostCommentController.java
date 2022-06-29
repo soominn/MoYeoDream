97c3b0eec44a4d314584398c978e8841770d130c
hanamanse <hana9589@gmail.com>
2022-06-30 03:23:56 +0900

    게시판 - boardWrite 카테고리 value 수정

--- spring/moyeodream/src/main/java/com/project/moyeodream/controller/PostCommentController.java
+++ spring/moyeodream/src/main/java/com/project/moyeodream/controller/PostCommentController.java
@@ -1,29 +1,53 @@
 package com.project.moyeodream.controller;
 
 import com.project.moyeodream.domain.vo.PostCommentVO;
+import com.project.moyeodream.service.PostCommentServiceImpl;
+import com.project.moyeodream.service.PostServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/postcomment/*")
+@RequestMapping("/postReply/*")
 public class PostCommentController {
+
+    private final PostCommentServiceImpl postCommentService;
+
     // 댓글 작성
     @PostMapping("register")
-    public void postCommentWrite(PostCommentVO postCommentVO){ }
+    public void postCommentWrite(@RequestBody PostCommentVO postCommentVO){
+        log.info("---------------------------------------");
+        log.info("reply register Controller......");
+        log.info("---------------------------------------");
+
+        postCommentService.registerReply(postCommentVO);
+        log.info(postCommentVO.getPostCommentNumber() + "번 댓글 등록 성공");
+    }
 
     // 댓글 수정
     @PostMapping("modify")
-    public void postCommentUpdate(PostCommentVO postCommentVO){ }
+    public void postCommentUpdate(@RequestBody PostCommentVO postCommentVO){
+        log.info("---------------------------------------");
+        log.info("reply modify Controller......");
+        log.info("---------------------------------------");
+
+        postCommentService.modifyReply(postCommentVO);
+        log.info(postCommentVO.getPostCommentNumber() + "번 댓글 수정 성공");
+    }
 
     // 댓글 삭제
-    @GetMapping("remove")
-    public void postCommentRemove(Integer postCommentNumber){ }
+    @GetMapping("remove/{postCommentNumber}")
+    public void postCommentRemove(@PathVariable Integer postCommentNumber){
+        log.info("---------------------------------------");
+        log.info("reply remove Controller......");
+        log.info("---------------------------------------");
+
+        postCommentService.removeReply(postCommentNumber);
+        log.info(postCommentNumber+"번 댓글 삭제 완료");
+    }
 
     // 전체 댓글 목록 조회
     @GetMapping("list")
