b9d5b94f7c29a278dadf8d2c10a722376cc9d393
hanamanse <hana9589@gmail.com>
2022-07-03 19:26:11 +0900

    게시판 - 첨부파일 업로드

--- spring/moyeodream/src/main/java/com/project/moyeodream/controller/FileController.java
+++ spring/moyeodream/src/main/java/com/project/moyeodream/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.project.moyeodream.controller;
 
 import com.project.moyeodream.domain.dao.FileDAO;
+import com.project.moyeodream.domain.vo.FileVO;
 import com.project.moyeodream.domain.vo.JobpostingDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -82,5 +83,40 @@ public class FileController {
     public void deleteCompanyLogo(String path){
         File file = new File("C:/upload/logo/", path);
         if(file.exists()) {file.delete();}
+    }
+
+    // 자유게시판 이미지 업로드
+    @PostMapping("/uploadPostFile")
+    @ResponseBody
+    public void uploadPostImage(MultipartFile[] files) throws Exception {
+        log.info("--------------------------------------------");
+        log.info(files.toString());
+
+        List<FileVO> fileList = new ArrayList<>();
+
+        String rootDirectory = "C:/upload/post";
+        File uploadDirectory = new File(rootDirectory, getDateDirectory());
+        if (!uploadDirectory.exists()) {
+            uploadDirectory.mkdirs();
+        }
+
+        UUID uuid = UUID.randomUUID();
+
+        for(MultipartFile file : files){
+            log.info("--------------------------------------------");
+            log.info("upload file name : "+ file.getOriginalFilename());
+            log.info("upload.file size : " + file.getSize() );
+
+            // 뷰로 보낼 파일객체
+            FileVO fileVO = new FileVO();
+
+            String fileName = uuid.toString() + "_" + file.getOriginalFilename();
+            fileVO.setFileName(fileName);
+
+            File saveFile = new File(uploadDirectory, fileName);
+            file.transferTo(saveFile);
+            fileVO.setUploadDirectory(getDateDirectory());
+            log.info("여기까지 완료");
+        }
     }
 }
